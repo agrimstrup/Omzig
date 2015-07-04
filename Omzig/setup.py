b54983e642cee04e8c8cb85d98ad630970e329aa
agrimstrup <ArtMakingWorking>
2015-07-04 15:14:43 -0600

    Created scaffolding for the Python package created by the project.

---
+++ Omzig/setup.py
@@ -0,0 +1,15 @@
+__author__ = 'arne'
+
+from distutils.core import setup
+
+setup(
+    name='Omzig',
+    version='0.1',
+    packages=['Omzig'],
+    url='www.omzig.org',
+    license='GPL3',
+    author='Carson McLean, Yakov Lipkovich, Arne Grimstrup',
+    author_email='camclean@ualberta.ca, yakov@live.com, agrimstrup@gmail.com',
+    description='Accessible User Interface to Kodi Media Center',
+    packages=['omzig']
+)
